4359c64b1634de0fd8979e89f376f58904a656d7
Gabriel Jacobina <sgabrieljacobina@gmail.com>
2020-11-22 01:08:06 -0300

    Add Lancamento

---
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -0,0 +1,37 @@
+package com.example.algamoney.api.service;
+
+import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.PessoaRepository;
+import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LancamentoService {
+
+    @Autowired
+    private LancamentoRepository lancamentoRepository;
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public Lancamento buscarLancamentoPorCodigo(Long codigo) {
+        Lancamento lancamento = lancamentoRepository.findOne(codigo);
+        if (lancamento == null) {
+            throw new EmptyResultDataAccessException(1);
+        }
+        return lancamento;
+    }
+
+    public Lancamento salvar(Lancamento lancamento) {
+        Pessoa pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
+        if (pessoa == null || pessoa.isInativo()) {
+            throw new PessoaInexistenteOuInativaException();
+        }
+
+        return lancamentoRepository.save(lancamento);
+    }
+}
